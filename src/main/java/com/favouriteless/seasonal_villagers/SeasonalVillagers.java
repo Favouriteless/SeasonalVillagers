14dbc340b1e55126ad9b28abbbcf48830a023226
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2022-10-07 14:03:42 +0100

    Initial commit

---
+++ src/main/java/com/favouriteless/seasonal_villagers/SeasonalVillagers.java
@@ -0,0 +1,19 @@
+package com.favouriteless.seasonal_villagers;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(SeasonalVillagers.MOD_ID)
+public class SeasonalVillagers
+{
+
+    public static final String MOD_ID = "seasonal_villagers";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SeasonalVillagers()
+    {
+
+    }
+
+}
